ca8bdf5de6e073f2e3fccdb9c44334d4668c6c21
Medetcan Diler <medetdiler1996@gmail.com>
2023-05-04 23:00:11 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // code your solution here
+const record = [
+  {year: "2018", result: "L"},
+  {year: "2017", result: "W"},
+  {year: "2016", result: "N/A"}
+  //...
+]
+
+function superbowlWin(record){
+  const winningYear = record.find( item => item.result === 'W')
+  return winningYear ? winningYear.year : undefined;
+}
+
+console.log(superbowlWin(record))
